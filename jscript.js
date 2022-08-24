8bd361feb3b2be4fad9551df7c6c13524dcaaf66
Paul-Arpita <arpi.paul1304058@gmail.com>
2022-08-24 10:46:41 +0600

    updated

--- jscript.js
+++ jscript.js
@@ -46,25 +46,30 @@ function totalPlayer(element) {
 }
 
 let perPlayerInput = document.getElementById('per-player-input').value;
-// console.log(perPlayerInput);
+console.log(perPlayerInput);
 let managerInput = document.getElementById('manager-input').value;
 // console.log(managerInput);
 let coachInput = document.getElementById('coach-input').value;
-
+let coachInputInt = parseInt(coachInput);
 document.getElementById('calculate-btn').addEventListener('click', function () {
-
+    let perPlayerInput = document.getElementById('per-player-input').value;
     let perPlayerExpense = (displayArray.length) * parseInt(perPlayerInput);
-
+    console.log(parseInt(perPlayerInput));
     let playersExpense = document.getElementById('players-expense');
-    playersExpense.innerText = perPlayerExpense;
+    playersExpense.innerText = parseInt(perPlayerExpense);
+    // console.log(parseFloat(playersExpense.innerText))
     return perPlayerExpense;
 })
 
 document.getElementById('calculate-total').addEventListener('click', function () {
+    let perPlayerInput = document.getElementById('per-player-input').value;
+    let managerInput = document.getElementById('manager-input').value;
+
+    let coachInput = document.getElementById('coach-input').value;
 
     let perPlayerExpense = (displayArray.length) * parseInt(perPlayerInput);
     let totalBudget = parseInt(perPlayerExpense) + parseInt(managerInput) + parseInt(coachInput);
     let total = document.getElementById('total');
 
-    total.innerText = parseInt(totalBudget);
+    total.innerText = totalBudget;
 })
